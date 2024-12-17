5358bea139ee1858488f5bd50ca53673e6fa248c
unknown <baysyn.nn@gmail.com>
2024-12-17 23:14:22 +0600

    homework_11 todolist

--- todolist/views.py
+++ todolist/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def todo_list(request):
-    pass
+    return HttpResponse("Список всех задач.")
 
 def todo_detail(request, todo_id):
-    pass
+    return HttpResponse(f"Детальная информация о задаче {todo_id}.")
 
 
 def todo_create(request):
-    pass
-
-def todo_update(request, todo_id):
-    pass
+    return HttpResponse("Создание новой задачи.")
 
 
 def todo_delete(request, todo_id):
+    return HttpResponse(f"Удаление задачи {todo_id}.")
+
+
+def todo_edit(request, todo_id):
+    return HttpResponse(f"Редактирование задачи {todo_id}.")
+
+
+def todo_update(request, todo_id):
     pass
