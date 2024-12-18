d63f09acd2aa1c4183bd5f8039667100e786f998
unknown <baysyn.nn@gmail.com>
2024-12-18 17:53:21 +0600

    lesson_22 files

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
-from todolist.models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'priority', 'is_completed')
-    search_fields = ('title', 'description')
-
-    
+# Register your models here.
