5effaae75fbc413211bf2d38f69f3df31a3ac281
unknown <baysyn.nn@gmail.com>
2025-01-16 15:38:02 +0600

    homework_17 done

--- post_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/todo_create/"
-data = {"title": "Test Task", "description": "This is a test task.", "completed": False}
-
-response = requests.post(url, json=data)
-
-print(response.status_code)
-print(response.json())
