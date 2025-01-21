d4f28b9efe09333adcaaef4cefecd98ad8f3d1db
unknown <baysyn.nn@gmail.com>
2025-01-21 16:34:19 +0600

    homework_36 done

--- bboard/templatetags/filtertags.py
+++ bboard/templatetags/filtertags.py
@@ -20,3 +20,32 @@ def lst(sep, *args):
 @register.inclusion_tag('tags/ulist.html')
 def ulist(*args):
     return {'items': args}
+
+# функция обрезания строки
+@register.filter
+def cut_half(value):
+    if isinstance(value, str):
+        return value[:len(value)//2]
+    return value
+
+#  Функция разделяет строку по указанному сепаратору и возвращает массив
+@register.simple_tag
+def split_string(string, separator):
+    if isinstance(string, str):
+        return string.split(separator)
+    return []
+
+# Преобразование строки в нижний регистр
+@register.filter
+def to_lower(value):
+    if isinstance(value, str):
+        return value.lower()
+    return value
+
+# Удаление пробелов в строке
+@register.filter
+def delete_space(value):
+    if isinstance(value, str):
+        return value.replace(" ", "")
+    return value
+
