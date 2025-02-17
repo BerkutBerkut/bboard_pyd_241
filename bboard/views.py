9736dbe3a565a1e0207f5de93910b92aadede882
unknown <baysyn.nn@gmail.com>
2025-02-17 16:34:55 +0600

    lesson_43 files

--- bboard/views.py
+++ bboard/views.py
@@ -28,6 +28,7 @@ from django.views.generic.edit import CreateView, FormView, UpdateView, DeleteVi
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_headers, vary_on_cookie
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -603,17 +604,36 @@ def all_users_group_view(request):
 ### DRF ###
 ###########
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def api_rubrics(request):
     if request.method == 'GET':
         rubrics = Rubric.objects.all()
         serializer = RubricSerializer(rubrics, many=True)
-        # return JsonResponse(serializer.data, safe=False)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = RubricSerializer(data=request.data)
 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET"])
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
 def api_rubric_detail(request, pk):
     rubric = Rubric.objects.get(pk=pk)
-    serializer = RubricSerializer(rubric)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = RubricSerializer(rubric)
+        return Response(serializer.data)
+    elif request.method == 'PUT' or request.method == 'PATCH':
+        serializer = RubricSerializer(rubric, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,
+                         status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        rubric.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
